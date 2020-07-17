d50ec3e97daa7b4a3689ae1af369f7b130385022
Seth Cox <Sethcox16@yahoo.com>
2020-07-17 15:39:34 -0400

    Names file working, run time 0.125 seconds

--- names/names.py
+++ names/names.py
@@ -1,11 +1,12 @@
+from BST import BSTNode
 import time
+
 
 start_time = time.time()
 
 f = open('names_1.txt', 'r')
 names_1 = f.read().split("\n")  # List containing 10000 names
 f.close()
-
 f = open('names_2.txt', 'r')
 names_2 = f.read().split("\n")  # List containing 10000 names
 f.close()
@@ -13,10 +14,19 @@ f.close()
 duplicates = []  # Return the list of duplicates in this data structure
 
 # Replace the nested for loops below with your improvements
-for name_1 in names_1:
-    for name_2 in names_2:
-        if name_1 == name_2:
-            duplicates.append(name_1)
+list_one = BSTNode(names_1[0])
+
+for name in names_1:
+    list_one.insert(name)
+
+for name in names_2:
+        if list_one.contains(name):
+            duplicates.append(name)
+
+
+# for name_1 in names_1: 
+#     if name_1 in names_2:
+#         duplicates.append(name_1)
 
 end_time = time.time()
 print (f"{len(duplicates)} duplicates:\n\n{', '.join(duplicates)}\n\n")
